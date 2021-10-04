37c5e2e3c291cb7dd2655203b9465ab706c7b6e6
Jirka Kremser <jiri.kremser@gmail.com>
2021-10-04 16:51:16 +0200

    Fix relative paths
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- generate.sh
+++ generate.sh
@@ -3,6 +3,7 @@
 # checks
 [[ $# != 2 ]] && echo "Usage: $0 <repo> <bundleVersion>" && exit 1
 
+echo PWD is $PWD
 git clone $1 && cd "$(basename "$_" .git)"
-BUNDLE_VERSION=$1 make -f ../Makefile bundle-full
+BUNDLE_VERSION=$1 make -f /Makefile bundle-full
 #echo "::set-output name=result::$a"
